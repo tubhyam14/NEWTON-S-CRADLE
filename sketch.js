3288ab1c065b751dab42a12824c5f8eae33c7f54
tubhyam14 <77627781+tubhyam14@users.noreply.github.com>
2021-09-26 10:07:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ bob5=new Bob(500,350,50);
 //bob6=new Bob(150,250,30);
  //fill ("brown");
   ground1 = new Ground(400,100,300,40);
-  sling1= new Suspender(bob1.body,ground1.body,-50*2,0);
+  sling1= new  Suspender(bob1.body,ground1.body,-50*2,0);
   sling2= new Suspender(bob2.body,ground1.body,-25*2,0);
   sling3= new Suspender(bob3.body,ground1.body,-0*2,0);
   sling4= new Suspender(bob4.body,ground1.body,25*2,0);
